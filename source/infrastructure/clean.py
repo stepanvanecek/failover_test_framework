a66456a436163397cbeaf5f37f45908722652036
Stepan Vanecek <stepan.vanecek@cloudandheat.com>
2016-05-08 23:48:15 +0200

    clean infrastructure

---
+++ source/infrastructure/clean.py
@@ -0,0 +1,26 @@
+import json
+import sys
+from build_nova import nova_client
+
+### MAIN
+if __name__ == "__main__":
+
+    try:
+        with open(sys.argv[2]) as json_data_file:
+            try:
+                tmpConfigData = json.load(json_data_file)
+            except ValueError:
+                print "wrong data format. should be json"
+                exit(1)
+    except IOError:
+        print "file not found/permission"
+        exit(1)
+
+    for vm in tmpConfigData['vms']:
+
+        nova = nova_client(tmpConfigData['creds']['os_username'], \
+                           tmpConfigData['creds']['os_tenant_name'], \
+                           sys.argv[1], \
+                           vm['deployment'])
+
+        nova.servers.delete(vm['id'])
