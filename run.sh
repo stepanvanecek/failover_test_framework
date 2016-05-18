aef7d563d7137d5264cfb28b54de2a1417530042
Stepan Vanecek <stepan.vanecek@cloudandheat.com>
2016-05-18 13:18:54 +0200

    include absolute path

--- run.sh
+++ run.sh
@@ -4,16 +4,17 @@ echo "Please enter your OpenStack Password: "
 read -sr OS_PASSWORD_INPUT
 
 
-SOURCE_DIR="source"
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/source"
 TMP_FILE="${SOURCE_DIR}/tmp_data.json"
-
-#build the infrastructure
+exit
+###build the infrastructure
 python $SOURCE_DIR/infrastructure/main.py $OS_PASSWORD_INPUT $TMP_FILE $1
 
-#perform the measurement
+###perform the measurement
 if [[ $? == 10 ]]; then
-	$SOURCE_DIR/measurement/program.o $OS_PASSWORD_INPUT $TMP_FILE $1 
+	$SOURCE_DIR/measurement/program.o $OS_PASSWORD_INPUT $TMP_FILE $1
 fi
 
+###remove the infrastructure
 python $SOURCE_DIR/infrastructure/clean.py $OS_PASSWORD_INPUT $TMP_FILE
-#rm $TMP_FILE
+rm $TMP_FILE
