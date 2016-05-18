e3d9d7dd43c51c71f9c4526844703a1b8e4ac7f7
Stepan Vanecek <stepan.vanecek@cloudandheat.com>
2016-05-18 22:08:09 +0200

    path independence

--- run.sh
+++ run.sh
@@ -3,18 +3,19 @@
 echo "Please enter your OpenStack Password: "
 read -sr OS_PASSWORD_INPUT
 
-
-SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/source"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR="${DIR}/source"
 TMP_FILE="${SOURCE_DIR}/tmp_data.json"
-exit
+
 ###build the infrastructure
-python $SOURCE_DIR/infrastructure/main.py $OS_PASSWORD_INPUT $TMP_FILE $1
+python $SOURCE_DIR/infrastructure/main.py $OS_PASSWORD_INPUT $TMP_FILE $DIR $1
 
 ###perform the measurement
 if [[ $? == 10 ]]; then
+	#echo "c prog"
 	$SOURCE_DIR/measurement/program.o $OS_PASSWORD_INPUT $TMP_FILE $1
 fi
 
 ###remove the infrastructure
 python $SOURCE_DIR/infrastructure/clean.py $OS_PASSWORD_INPUT $TMP_FILE
-rm $TMP_FILE
+rm $TMP_FILE  2> /dev/null
